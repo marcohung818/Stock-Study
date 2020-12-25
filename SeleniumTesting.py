a75fc3eb13b11b0e95418ec0dbe25ca6b7c2890b
Marco Hung <37731008+marcohung818@users.noreply.github.com>
2020-12-25 14:01:10 +0800

    25/12/2020

--- SeleniumTesting.py
+++ SeleniumTesting.py
@@ -8,32 +8,128 @@ import time
 import StockDataBase as db #import the Stock List
 chromeDriverPATH = "chromedriver.exe"
 driver = webdriver.Chrome(chromeDriverPATH)
-#nasdaqEarningUrlFormat = "https://www.nasdaq.com/market-activity/stocks/aapl/earnings"
+#nasdaqEarningUrlFormat = "https://www.nasdaq.com/market-activity/stocks/aapl/earnings" For the Quarterly EPS
+#nasdaqFinancialUrlFormat = "https://www.nasdaq.com/market-activity/stocks/aapl/financials" For the Quarterly / Annually Financial
+dataFrameCols = ["Fiscal Period", "Quarterly EPS Score", "Quarterly EPS Changed %", "Annual EPS Score", "Annual EPS Changed %", "Quarterly Revenue Score", "Quarterly Revenue Changed %", "Annual Revenue Score", "Annual Revenue Changed %", "Quarterly Gross Profit Score", "Quarterly Gross Profit Changed %", "Annual Gross Profit Score", "Annual Gross Profit Changed %"]
+#The Most important Data output
+output = pd.DataFrame(columns=dataFrameCols)
 
-
-def getStockData(symbol):
+#Program Function
+#The Main Get Stock Function
+def getStockDataMain(symbol):
+    quarterlyEpsChanged, quarterlyEpsScore = getStockQuarterlyEPS(symbol)
+    annuallyRevenueChanged, annuallyRevenueScore = getStockAnnualRevenue(symbol)
+    print(annuallyRevenueChanged)
+    print(annuallyRevenueScore)
+    #getStockQuarterlyEPS(symbol)
+    return
+#Get and Calculate the Quarterly Data
+#Get the Quarterly EPS -  return 1.EpsChanged 2.EpsScore
+def getStockQuarterlyEPS(symbol):
     driver.get("https://www.nasdaq.com/market-activity/stocks/" + str(symbol) + "/earnings")
-    #try:
-    mainPageLoaded = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "/html/body"))
-    )
-    epstbodyXpath = "/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[3]/div/div/table/tbody"
-    epstbodies = mainPageLoaded.find_elements_by_xpath(epstbodyXpath)
-    for i in range(2, 5):
-        print((mainPageLoaded.find_element_by_xpath(epstbodyXpath + "[1]/" +"tr/th[" + str(i) + "]" )).text)
-    #print(len(tbody))
-    #except:
-        #driver.quit()
-        #print("error")
+    EpsData = []
+    try:
+        mainPageLoaded = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body"))
+        )
+        #Xpath = "/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[3]/div/div/table/tbody[2]/tr[1]/td[2]"
+        Xpth = "/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[3]/div/div/table/tbody[2]/tr["
+        #Get the Value of the EPS
+        for i in range(1, 5):
+            EpsData.append(float(mainPageLoaded.find_element_by_xpath(Xpth + str(i) +"]/td[2]").text))
+    except:
+        driver.quit()
+        print("Error in QuarterlyEPS")
+    return countingMachine(EpsData)
+
+#Get and Calculate the Annual Data
+#Get the Annually Revenue - return 1.RevenueChanged 2.RevenueScore
+def getStockAnnualRevenue(symbol):
+    driver.get("https://www.nasdaq.com/market-activity/stocks/" + str(symbol) + "/financials")
+    RevenueData = []
+    try:
+        mainPageLoaded = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body"))
+        )
+        #Xpath = "/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[1]/div/div[3]/div[2]/div[1]/table/tbody/tr[1]/td[1]"
+        Xpth = "/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[1]/div/div[3]/div[2]/div[1]/table/tbody/tr[1]/td["
+        #Get the Value of the Revenue
+        for i in range(1, 5):
+            RevenueData.append(float(removeSpcialCharacter(mainPageLoaded.find_element_by_xpath(Xpth + str(i) +"]").text)))
+    except:
+        driver.quit()
+        print("Error in AnnualRevenue")
+    return countingMachine(RevenueData)
+#Pass the data - return 1. Percentage Changed 2. Score
+def countingMachine(data):
+    print(data)
+    score = []
+    changedPercentage = []
+    #Calculate the change
+    for i in range(0, 3):
+        Changed = (data[i] - data[i+1]) / data[i+1]
+        changedPercentage.append(Changed)
+    #Calculate the Score
+    if(changedPercentage[0] >= 0):
+        score.append(4)
+    else:
+        score.append(0)
+    if(changedPercentage[1] >= 0):
+        score.append(2)
+    else:
+        score.append(0)
+    if(changedPercentage[2] >= 0):
+        score.append(1)
+    else:
+        score.append(0)
+    return changedPercentage, score
+#Cancel the Special Character - return a string can transfer to float
+def removeSpcialCharacter(string):
+    outputString = ""
+    for character in string:
+        if character.isalnum():
+            outputString += character
+    return outputString
+
+#Check Mode Function
+def checkModeMain():
+    while(1):
+        print("")
+        print("checkeps - Check the EPS data location")
+        print("exit - Exit the Check Mode")
+        checkModeMainInput = input()
+        if(checkModeMainInput == "checkeps"):
+            checkStockQuarterlyEPS()
+        else:
+            return
+#Check the Quarterly EPS Location
+def checkStockQuarterlyEPS():
+    driver.get("https://www.nasdaq.com/market-activity/stocks/aapl/earnings")
+    try:
+        mainPageLoaded = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body"))
+        )
+        #Xpath = "/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[3]/div/div/table/tbody[2]/tr[1]/td[2]"
+        Xpth = "/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[3]/div/div/table/tbody[2]/tr["
+        #Get the Value of the EPS
+        for i in range(1, 5):
+            print(float(mainPageLoaded.find_element_by_xpath(Xpth + str(i) +"]/td[2]").text))
+    except:
+        driver.quit()
+        print("error")
     return
 
 def main():
     while(1):
-        print("Input the Stock Symbol or quit the program by \"exit\"")
+        print("Stock Symbol - Run the program")
+        print("check - Check Mode")
+        print("exit - Exit the Program")
         mainInput = input()
-        if(mainInput != "exit"):
-            getStockData(mainInput)
-        else:
+        if(mainInput == "exit"):
             return
+        elif(mainInput == "check"):
+            checkModeMain()
+        else:
+            getStockDataMain(mainInput)
 
 main()
